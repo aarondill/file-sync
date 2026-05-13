088651a0f21a47760bfdad7ecbd82c49a4869966
Aaron Dill <aaronsacks2006@gmail.com>
2026-05-12 21:48:49 -0500

    fix(cpp): serial/deserial to big-endian

--- cpp/src/lib/protocol/serial.cpp
+++ cpp/src/lib/protocol/serial.cpp
@@ -1,5 +1,4 @@
 #include "serial.h"
-#include <utility>
 namespace protocol {
 std::expected<CBuffer, serror> deserialize(std::byte &out, CBuffer buf) {
   if (buf.empty()) return std::unexpected(serror::NOT_ENOUGH_DATA);
@@ -37,28 +36,28 @@ std::expected<Buffer, serror> serialize(const Buffer buf, const uint8_t &in) {
 
 std::expected<CBuffer, serror> deserialize(uint32_t &out, const CBuffer buf) {
   if (buf.size() < sizeof(out)) return std::unexpected(serror::NOT_ENOUGH_DATA);
-  out = static_cast<uint32_t>(buf[0]) | static_cast<uint32_t>(buf[1]) << 8 |
-        static_cast<uint32_t>(buf[2]) << 16 | static_cast<uint32_t>(buf[3]) << 24;
+  out = static_cast<uint32_t>(buf[0]) << 24 | static_cast<uint32_t>(buf[1]) << 16 |
+        static_cast<uint32_t>(buf[2]) << 8 | static_cast<uint32_t>(buf[3]);
   return buf.subspan(sizeof(out));
 }
 std::expected<Buffer, serror> serialize(Buffer buf, const uint32_t &in) {
   if (buf.size() < sizeof(in)) return std::unexpected(serror::NOT_ENOUGH_DATA);
-  buf[0] = static_cast<std::byte>(in);
-  buf[1] = static_cast<std::byte>(in >> 8);
-  buf[2] = static_cast<std::byte>(in >> 16);
-  buf[3] = static_cast<std::byte>(in >> 24);
+  buf[0] = static_cast<std::byte>(in >> 24);
+  buf[1] = static_cast<std::byte>(in >> 16);
+  buf[2] = static_cast<std::byte>(in >> 8);
+  buf[3] = static_cast<std::byte>(in);
   return buf.subspan(sizeof(in));
 }
 
 std::expected<CBuffer, serror> deserialize(uint16_t &out, CBuffer buf) {
   if (buf.size() < sizeof(out)) return std::unexpected(serror::NOT_ENOUGH_DATA);
-  out = static_cast<uint16_t>(buf[0]) | static_cast<uint16_t>(buf[1]) << 8;
+  out = static_cast<uint16_t>(buf[0]) << 8 | static_cast<uint16_t>(buf[1]);
   return buf.subspan(sizeof(out));
 }
 std::expected<Buffer, serror> serialize(Buffer buf, const uint16_t &in) {
   if (buf.size() < sizeof(in)) return std::unexpected(serror::NOT_ENOUGH_DATA);
-  buf[0] = static_cast<std::byte>(in);
-  buf[1] = static_cast<std::byte>(in >> 8);
+  buf[0] = static_cast<std::byte>(in >> 8);
+  buf[1] = static_cast<std::byte>(in);
   return buf.subspan(sizeof(in));
 }
 } // namespace protocol

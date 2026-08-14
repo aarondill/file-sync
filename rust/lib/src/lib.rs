66e73ceac5b2c96878d30f63ef5e56216210c181
Aaron Dill <aaronsacks2006@gmail.com>
2026-08-13 21:00:53 -0500

    feat(rust): read/write message

--- rust/lib/src/lib.rs
+++ rust/lib/src/lib.rs
@@ -1,7 +1,4 @@
-// TODO: remove me
-#![allow(unused_variables)]
-#![allow(unused_imports)]
-
+use std::io::Read;
 pub mod client_connect;
 pub mod download_file;
 pub mod download_message;
@@ -10,3 +7,20 @@ pub mod error;
 pub mod file_hash;
 pub mod protocol;
 pub mod variable_length_string;
+use crate::protocol::{Deserialize, Serialize};
+
+pub fn read_message(reader: &mut dyn Read) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+    let len = u16::deserialize(reader)? as usize;
+    let mut buffer = vec![0u8; len];
+    reader.read_exact(&mut buffer)?;
+    Ok(buffer)
+}
+pub fn write_message(
+    writer: &mut dyn std::io::Write,
+    message: &[u8],
+) -> Result<(), Box<dyn std::error::Error>> {
+    let len: u16 = message.len().try_into()?;
+    len.serialize(writer)?;
+    writer.write_all(message)?;
+    Ok(())
+}

93fa41eacb02f619c7411ce01fe270e16cffc0b1
Aaron Dill <aaronsacks2006@gmail.com>
2026-08-18 22:09:54 -0500

    feat(rust): client

--- rust/client/src/main.rs
+++ rust/client/src/main.rs
@@ -1,15 +1,23 @@
 // gethostname is not stable yet, i could use a crate, but i'd rather use the nightly feature
 #![feature(gethostname)]
 
+use std::collections::HashMap;
+use std::io::Read;
 use std::path::Path;
 use std::process::ExitCode;
+use std::sync::atomic::AtomicBool;
 
+use futures::StreamExt;
 use lib::client_connect::{self, ClientConnect};
+use lib::download::download;
 use lib::file_info::FileInfo;
 use lib::protocol::write_message;
 use lib::serial::Serialize;
+use lib::upload::upload;
 use lib::variable_length_string::VariableLengthString;
 use tokio::net::TcpStream;
+use tokio::sync::mpsc;
+use tokio::{io, pin};
 
 // Initialize a client connect message
 fn init_connect_msg(upload: bool) -> ClientConnect {
@@ -20,9 +28,13 @@ fn init_connect_msg(upload: bool) -> ClientConnect {
     ClientConnect::new(flags, name)
 }
 
-fn update_list(directory: &Path, list: &mut Vec<FileInfo>) {
+async fn update_list(directory: &Path, list: &mut Vec<FileInfo>) {
     list.clear();
-    list.extend(FileInfo::read_list(directory));
+    let stream = FileInfo::read_list(directory);
+    pin!(stream);
+    while let Some(info) = stream.next().await {
+        list.push(info);
+    }
 }
 
 fn parse_args(iter: impl Iterator<Item = String>) -> (Vec<String>, Vec<String>) {
@@ -46,7 +58,7 @@ fn parse_args(iter: impl Iterator<Item = String>) -> (Vec<String>, Vec<String>) 
 async fn main() -> ExitCode {
     let mut global_list = Vec::<FileInfo>::new();
     // TODO: ctrl-c
-    // std::atomic_flag stop = false;
+    let stop = AtomicBool::new(false);
 
     let (args, opts) = parse_args(std::env::args());
     let should_upload = opts.contains(&"-u".to_string());
@@ -63,9 +75,9 @@ async fn main() -> ExitCode {
 
     // The server starts by sending an upload to the client unless the client
     // explicitly requests otherwise
-    let mut upload_pending = should_upload;
+    let upload_pending = AtomicBool::new(should_upload);
 
-    update_list(directory, &mut global_list); // update the list before starting the server
+    update_list(directory, &mut global_list).await; // update the list before starting the server
 
     let addr = server
         .rsplit_once(":")
@@ -78,61 +90,90 @@ async fn main() -> ExitCode {
         let msg = init_connect_msg(should_upload);
         let mut buf = Vec::with_capacity(4096);
         msg.serialize(&mut buf).expect("error serializing client connect message");
-        write_message(&mut connection, &buf).expect("error writing connect message");
+        write_message(&mut connection, &buf).await.expect("error writing connect message");
     }
 
-    //
-    //   constexpr int CONN_IND = 0, STDIN_IND = 1;
-    //   pollfd p_fds[2];
-    //   p_fds[CONN_IND] = {.fd = static_cast<int>(connection), .events = POLL_IN, .revents{}};
-    //   p_fds[STDIN_IND] = {.fd = STDIN_FILENO, .events = POLLIN, .revents{}};
-    //
-    //   std::unordered_map<char, std::pair<std::string_view, std::function<void()>>> commands{
-    //       {'q', {"quit", []() { stop.test_and_set(); }}},
-    //       {'u', {"upload", []() { upload_pending.test_and_set(); }}},
-    //       {'h',
-    //        {"help",
-    //         [&commands]() {
-    //           std::cout << "commands: ";
-    //           for (const auto &[c, v] : commands)
-    //             std::cout << c << ": " << v.first << std::endl;
-    //           std::cout << std::endl;
-    //         }}},
-    //   };
-    //
-    //   while (!stop.test()) {
-    //     if (!upload_pending.test()) {
-    //       p_fds[CONN_IND].revents = 0;
-    //       const int ret = poll(p_fds, std::size(p_fds), -1);
-    //       assert(ret != 0); // no timeout, so this should be true
-    //       // poll can still be interrupted by EINTR
-    //       if (ret < 0 && errno != EINTR) throw std::runtime_error(std::strerror(errno));
-    //
-    //       if (p_fds[STDIN_IND].revents & POLLIN) {
-    //         char c;
-    //         std::cin.get(c);
-    //         if (std::isspace(c)) { // ignore whitespace, we'll just read it next time around
-    //         } else if (commands.contains(c)) {
-    //           commands.at(c).second();
-    //         } else {
-    //           std::cerr << "unknown command: " << c << std::endl;
-    //         }
-    //       }
-    //
-    //       if (p_fds[CONN_IND].revents & POLLIN) {
-    //         download(connection, global_list, directory);
-    //         update_list(directory);
-    //       }
-    //     }
-    //
-    //     if (upload_pending.test()) {
-    //       if (p_fds[CONN_IND].revents & POLLIN)
-    //         throw std::runtime_error("upload pending while connection has data!");
-    //
-    //       update_list(directory); // files may change between downloads
-    //       upload(connection, global_list, directory);
-    //       upload_pending.clear();
-    //     }
-    //   }
+    let mut commands: HashMap<char, Box<dyn FnMut() + Send>> = HashMap::new();
+    commands.insert(
+        'q',
+        Box::new(|| {
+            stop.store(true, std::sync::atomic::Ordering::SeqCst);
+        }),
+    );
+    commands
+        .insert('u', Box::new(|| upload_pending.store(true, std::sync::atomic::Ordering::SeqCst)));
+    commands.insert(
+        'h',
+        Box::new(|| {
+            println!("commands: ");
+            println!("  q: quit");
+            println!("  u: upload");
+            println!("  h: help");
+            println!();
+        }),
+    );
+
+    let (send, mut recv) = mpsc::channel(32);
+    std::thread::spawn(move || {
+        // Handle stdin input
+        let handle = std::io::stdin().lock();
+        for c in handle.bytes() {
+            let Ok(c) = c else {
+                break;
+            };
+            send.blocking_send(c).unwrap();
+        }
+    });
+
+    while !stop.load(std::sync::atomic::Ordering::SeqCst) {
+        if !upload_pending.load(std::sync::atomic::Ordering::SeqCst) {
+            tokio::select! {
+                recv = recv.recv() => { // user input
+                    let Some(c) = recv else {
+                        eprintln!("connection closed");
+                        return ExitCode::FAILURE;
+                    };
+                    let c = c as char;
+                    if c.is_ascii_whitespace() {
+                        continue;
+                    }
+                    match commands.get_mut(&c) {
+                        Some(f) => f(),
+                        None => eprintln!("unknown command: {}", c),
+                    }
+                }
+                r = connection.readable() => {
+                    if let Err(e) = r {
+                        eprintln!("error reading from connection: {}", e);
+                        return ExitCode::FAILURE;
+                    }
+                    download(&mut connection, &global_list, directory).await.expect("download failed");
+                    update_list(directory, &mut global_list).await;
+                }
+            }
+        }
+
+        if upload_pending.load(std::sync::atomic::Ordering::SeqCst) {
+            let mut buf = [0];
+            match connection.try_read(&mut buf) {
+                Err(e) if e.kind() == io::ErrorKind::WouldBlock => {} // ok
+                Ok(0) => {
+                    eprintln!("connection closed");
+                    return ExitCode::FAILURE;
+                }
+                Ok(_) => {
+                    eprintln!("upload pending while connection has data!");
+                    return ExitCode::FAILURE;
+                }
+                Err(e) => {
+                    eprintln!("error reading from connection: {}", e);
+                    return ExitCode::FAILURE;
+                }
+            };
+            update_list(directory, &mut global_list).await; // files may change between downloads
+            upload(&mut connection, &global_list, directory).await.expect("upload failed");
+            upload_pending.store(false, std::sync::atomic::Ordering::SeqCst);
+        }
+    }
     return ExitCode::SUCCESS;
 }
